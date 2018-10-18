778618cc3ccf3a6249605ce2b60e81b2259f0c0c
ZackeryWelk <43837860+ZackeryWelk@users.noreply.github.com>
2018-10-19 10:45:08 +1100

    Added: - pause panel prefab

--- GhostMunch/Assets/Scripts/PauseMenu.cs
+++ GhostMunch/Assets/Scripts/PauseMenu.cs
@@ -4,9 +4,12 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
+
+
 public class PauseMenu : MonoBehaviour {
 
     public GameObject pausePanel;
+   
 	// Use this for initialization
 	void Start ()
     {
@@ -15,16 +18,16 @@ public class PauseMenu : MonoBehaviour {
 	// Update is called once per frame
 	void Update ()
     {
-        if (Input.GetKeyDown("escape") && !pausePanel.activeSelf)
+        if (Input.GetKeyDown("escape") && !pausePanel.activeSelf || Input.GetKeyDown("w") && !pausePanel.activeSelf)
         {
             pausePanel.SetActive(true);
             Time.timeScale = 0.0f;
         }
-        else if (Input.GetKeyDown("escape") && pausePanel.activeSelf)
+        else if (Input.GetKeyDown("escape") && pausePanel.activeSelf || Input.GetKeyDown("w") && pausePanel.activeSelf)
         {
             pausePanel.SetActive(false);
             Time.timeScale = 1.0f;
-        }	
+        }	    
 	}
 
     public void Resume()
